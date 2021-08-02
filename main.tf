2ab532534bab698b26b4c0f15361e0bfd864fff9
James Hale <james@hale.dev>
2021-08-01 21:22:52 -0400

    Update syntax

--- main.tf
+++ main.tf
@@ -1,19 +1,19 @@
 resource "digitalocean_droplet" "proxy_droplet_nyc1" {
   name      = "dns-proxy-nyc1"
   region    = "nyc1"
-  size      = "${var.droplet_size}"
-  image     = "${var.image_id}"
-  ssh_keys  = "${var.ssh_keys}"
-  user_data = "${data.template_file.userdata.rendered}"
+  size      = var.droplet_size
+  image     = var.image_id
+  ssh_keys  = var.ssh_keys
+  user_data = data.template_file.userdata.rendered
 }
 
 resource "digitalocean_droplet" "proxy_droplet_nyc3" {
   name      = "dns-proxy-nyc3"
   region    = "nyc3"
-  size      = "${var.droplet_size}"
-  image     = "${var.image_id}"
-  ssh_keys  = "${var.ssh_keys}"
-  user_data = "${data.template_file.userdata.rendered}"
+  size      = var.droplet_size
+  image     = var.image_id
+  ssh_keys  = var.ssh_keys
+  user_data = data.template_file.userdata.rendered
 }
 
 resource "digitalocean_floating_ip" "floating_ip_nyc1" {
@@ -25,21 +25,21 @@ resource "digitalocean_floating_ip" "floating_ip_nyc3" {
 }
 
 resource "digitalocean_floating_ip_assignment" "floating_ip_assignment_nyc1" {
-  ip_address = "${digitalocean_floating_ip.floating_ip_nyc1.id}"
-  droplet_id = "${digitalocean_droplet.proxy_droplet_nyc1.id}"
+  ip_address = digitalocean_floating_ip.floating_ip_nyc1.id
+  droplet_id = digitalocean_droplet.proxy_droplet_nyc1.id
 }
 
 resource "digitalocean_floating_ip_assignment" "floating_ip_assignment_nyc3" {
-  ip_address = "${digitalocean_floating_ip.floating_ip_nyc3.id}"
-  droplet_id = "${digitalocean_droplet.proxy_droplet_nyc3.id}"
+  ip_address = digitalocean_floating_ip.floating_ip_nyc3.id
+  droplet_id = digitalocean_droplet.proxy_droplet_nyc3.id
 }
 
 data "template_file" "userdata" {
-  template = "${file("${path.module}/userdata.tpl")}"
+  template = file("${path.module}/userdata.tpl")
 
   vars = {
-    dns_ip_1 = "${digitalocean_floating_ip.floating_ip_nyc1.ip_address}"
-    dns_ip_2 = "${digitalocean_floating_ip.floating_ip_nyc3.ip_address}"
+    dns_ip_1 = digitalocean_floating_ip.floating_ip_nyc1.ip_address
+    dns_ip_2 = digitalocean_floating_ip.floating_ip_nyc3.ip_address
   }
 }
 
@@ -47,53 +47,49 @@ resource "digitalocean_firewall" "proxy" {
   name = "allow-dns-https"
 
   droplet_ids = [
-    "${digitalocean_droplet.proxy_droplet_nyc1.id}",
-    "${digitalocean_droplet.proxy_droplet_nyc3.id}",
+    digitalocean_droplet.proxy_droplet_nyc1.id,
+    digitalocean_droplet.proxy_droplet_nyc3.id,
   ]
 
-  inbound_rule = [
-    {
-      protocol         = "tcp"
-      port_range       = "22"
-      source_addresses = ["${var.admin_ips}"]
-    },
-    {
-      protocol         = "tcp"
-      port_range       = "80"
-      source_addresses = ["${var.admin_ips}", "${var.user_ips}"]
-    },
-    {
-      protocol         = "tcp"
-      port_range       = "443"
-      source_addresses = ["${var.admin_ips}", "${var.user_ips}"]
-    },
-    {
-      protocol         = "udp"
-      port_range       = "53"
-      source_addresses = ["${var.admin_ips}", "${var.user_ips}"]
-    },
-  ]
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "22"
+    source_addresses = [jsonencode(var.user_ips)]
+  }
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "80"
+    source_addresses = [jsonencode(var.admin_ips), jsonencode(var.user_ips)]
+  }
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "443"
+    source_addresses = [jsonencode(var.admin_ips), jsonencode(var.user_ips)]
+  }
+  inbound_rule {
+    protocol         = "udp"
+    port_range       = "53"
+    source_addresses = [jsonencode(var.admin_ips), jsonencode(var.user_ips)]
+  }
 
-  outbound_rule = [
-    {
-      protocol              = "tcp"
-      port_range            = "53"
-      destination_addresses = ["0.0.0.0/0"]
-    },
-    {
-      protocol              = "udp"
-      port_range            = "53"
-      destination_addresses = ["0.0.0.0/0"]
-    },
-    {
-      protocol              = "tcp"
-      port_range            = "80"
-      destination_addresses = ["0.0.0.0/0"]
-    },
-    {
-      protocol              = "tcp"
-      port_range            = "443"
-      destination_addresses = ["0.0.0.0/0"]
-    },
-  ]
+  outbound_rule {
+    protocol              = "tcp"
+    port_range            = "53"
+    destination_addresses = ["0.0.0.0/0"]
+  }
+  outbound_rule {
+    protocol              = "udp"
+    port_range            = "53"
+    destination_addresses = ["0.0.0.0/0"]
+  }
+  outbound_rule {
+    protocol              = "tcp"
+    port_range            = "80"
+    destination_addresses = ["0.0.0.0/0"]
+  }
+  outbound_rule {
+    protocol              = "tcp"
+    port_range            = "443"
+    destination_addresses = ["0.0.0.0/0"]
+  }
 }
--- variables.tf
+++ variables.tf
@@ -1,23 +1,23 @@
 variable "admin_ips" {
-  type        = "list"
+  type        = list
   description = "List of IPv4 addresses allowed to query DNS, proxy HTTP/S connections, and SSH to the proxy instances."
   default     = []
 }
 
 variable "user_ips" {
-  type        = "list"
+  type        = list
   description = "List of IPv4 addresses allowed to query DNS and proxy HTTP/S connections."
   default     = []
 }
 
 variable "ssh_keys" {
-  type        = "list"
+  type        = list
   description = "List of DigitalOcean identifiers for a SSH keys to use"
 }
 
 variable "image_id" {
   description = "ID of the OS image to use for the instances."
-  default     = "ubuntu-18-04-x64"
+  default     = "ubuntu-20-04-x64"
 }
 
 variable "droplet_size" {
